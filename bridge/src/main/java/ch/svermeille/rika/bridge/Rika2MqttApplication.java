fb3776aa754f4cdd17b3c5acf8756e39f2446f93
Sebastien Vermeille <sebastien.vermeille@geo-satis.com>
2022-10-03 20:12:52 +0200

    Add a few unit tests

--- bridge/src/main/java/ch/svermeille/rika/bridge/Rika2MqttApplication.java
+++ bridge/src/main/java/ch/svermeille/rika/bridge/Rika2MqttApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = {"ch.svermeille.rika.*"})
 public class Rika2MqttApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Rika2MqttApplication.class, args);
 	}
-
 }
