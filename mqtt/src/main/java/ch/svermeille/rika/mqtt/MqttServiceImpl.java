5777f9154d3dadd2d680341ba6a6984394ee2c96
Sebastien Vermeille <sebastien.vermeille@gmail.com>
2023-05-02 22:03:11 +0200

    Add copyrights

--- mqtt/src/main/java/ch/svermeille/rika/mqtt/MqttServiceImpl.java
+++ mqtt/src/main/java/ch/svermeille/rika/mqtt/MqttServiceImpl.java
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2023 Sebastien Vermeille and contributors.
+ *
+ * Use of this source code is governed by an MIT
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
 package ch.svermeille.rika.mqtt;
 
 import ch.svermeille.rika.mqtt.configuration.MqttConfiguration;
