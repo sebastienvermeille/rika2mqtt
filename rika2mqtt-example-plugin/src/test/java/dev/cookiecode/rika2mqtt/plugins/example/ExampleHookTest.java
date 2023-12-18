07fa0b7e4f605c3036b6f15be715f54e7496bf1f
Sebastien Vermeille <sebastien.vermeille@gmail.com>
2023-12-18 15:42:00 +0100

    Refactore/add some tests

--- rika2mqtt-example-plugin/src/test/java/dev/cookiecode/rika2mqtt/plugins/example/ExampleHookTest.java
+++ rika2mqtt-example-plugin/src/test/java/dev/cookiecode/rika2mqtt/plugins/example/ExampleHookTest.java
@@ -3,7 +3,6 @@ package dev.cookiecode.rika2mqtt.plugins.example;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class ExampleHookTest {
 
