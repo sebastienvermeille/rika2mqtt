06cdd5f0f8e975fca087f7a37e7748a11a23e1a2
Sebastien Vermeille <sebastien.vermeille@gmail.com>
2023-12-18 13:36:07 +0100

    Add unit tests for API module

---
+++ plugins-api/src/test/java/dev/cookiecode/rika2mqtt/plugins/api/v1/model/TimeRangeTest.java
@@ -0,0 +1,48 @@
+/*
+ * The MIT License
+ * Copyright © 2022 Sebastien Vermeille
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package dev.cookiecode.rika2mqtt.plugins.api.v1.model;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+/** Test class */
+class TimeRangeTest {
+
+  @Test
+  void toStringShouldReturnADeterminedFormat() {
+
+    // GIVEN
+    final var from = TimeDefinition.builder().hours(12).minutes(5).build();
+
+    final var to = TimeDefinition.builder().hours(15).minutes(30).build();
+
+    final var timeRange = TimeRange.builder().from(from).to(to).build();
+
+    // WHEN
+    final var result = timeRange.toString();
+
+    // THEN
+    assertThat(result).isEqualTo("12:05 - 15:30");
+  }
+}
